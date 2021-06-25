2cd19aa1d796d712e863409d6a2ab872005dc03e
RayadoxAV <49849531+RayadoxAV@users.noreply.github.com>
2021-06-24 19:00:47 -0600

    Fórmulas

--- views/js/calcularPrecio.js
+++ views/js/calcularPrecio.js
@@ -3,21 +3,25 @@ const precioVidrioBase = 25.33875;
 const precioUnidadTMullion = 11.77;
 const constanteTMullion = 0.8771929825;
 
-const regexAncho = /An/gmi;
-const regexAlto = /Al/gmi;
-const regexPrecioVidrioBase = /PV/gmi;
-const regexPrecioVidrio = /PV2/gmi;
-const TMullion = /T-Mullion/gmi;
+const regexAncho = /\bAn\b/gmi;
+const regexAncho2 = /\bAn2\b/gmi;
+const regexAlto = /\bAl\b/gmi;
+const regexAlto2 = /\bAl2\b/gmi;
+const regexPrecioVidrioBase = /\bPV\b/gmi;
+const regexPrecioVidrio2 = /\bPV2\b/gmi;
+const TMullion = /\bT-Mullion\b/gmi;
 
-const formulaEjemplo = '((An / 3 + Al) * PV) * 3 + (T-Mullion) * 2';
+const formulaEjemplo = '((An / 3 + Al) * PV) * 3 + (T-Mullion) * 2 + PV2';
 
 function analizadorLexico(formula) {
   let stringFinal = formula;
 
   stringFinal = stringFinal.replaceAll(regexAncho, 'rutaVentana.dimensionAncho');
+  stringFinal = stringFinal.replaceAll(regexAncho2, 'rutaVentana.dimensionAncho2');
   stringFinal = stringFinal.replaceAll(regexAlto, 'rutaVentana.dimensionAlto');
   stringFinal = stringFinal.replaceAll(regexPrecioVidrioBase, `determinarPrecioVidrio('Básica', 'Fija', 'Sencillo Claro')`);
-  stringFinal = stringFinal.replaceAll(regexPrecioVidrio, `determinarPrecioVidrio()`);
+  stringFinal = stringFinal.replaceAll(regexPrecioVidrio2, `determinarPrecioVidrio('Básica', 'Fija', 'Sencillo Claro')`)
+  stringFinal = stringFinal.replaceAll(regexPrecioVidrioBase, `determinarPrecioVidrio()`);
   stringFinal = stringFinal.replaceAll(TMullion, `(rutaVentana.dimensionAncho / ${constanteTMullion}) * ${precioUnidadTMullion}`);
 
   rutaVentana = {
